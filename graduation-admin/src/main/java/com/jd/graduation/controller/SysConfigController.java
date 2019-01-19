b998dc9d2a9133bdfd067bb4c749e23164447a1f
jingdian <461320432@qq.com>
2019-01-19 10:41:47 +0800

    评论相关

--- graduation-admin/src/main/java/com/jd/graduation/controller/SysConfigController.java
+++ graduation-admin/src/main/java/com/jd/graduation/controller/SysConfigController.java
@@ -1,6 +1,7 @@
 package com.jd.graduation.controller;
 
 import com.jd.graduation.Impl.SysConfigServiceImpl;
+import com.jd.graduation.service.AuthenticationService;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,5 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class SysConfigController extends BaseController{
     @Autowired
     private SysConfigServiceImpl sysConfigService;
+    @Autowired
+    private AuthenticationService authenticationService;
 
+    // 读取修改配置相关，包括售后服务期限，运费相关，可写可不写
 }
