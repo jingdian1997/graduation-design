65d5f0db4e36ea1b14486f67daf05e0c1eceba2e
jingdian <461320432@qq.com>
2019-01-23 11:14:27 +0800

    配置pom

--- graduation-api/src/main/java/com/jd/graduation/mapper/UserPurseMapper.java
+++
@@ -1,7 +0,0 @@
-package com.jd.graduation.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.jd.graduation.DO.UserPurseDO;
-
-public interface UserPurseMapper extends BaseMapper<UserPurseDO> {
-}
